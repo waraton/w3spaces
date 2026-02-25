a5b231b66006c851b26077267a5b6533822d80e1
waraton <bensonwaran@gmail.com>
2026-02-25 12:50:49 +0300

    modified:   about.html modified:   index.html new file:   resets.css modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tabs = document.querySelectorAll(`nav ul li`)
+const tabs = document.querySelectorAll(`nav ul li`);
 
 /* tabs.forEach((tab)=>{
     tab.addEventListener(`click`, ()=>{
@@ -7,48 +7,52 @@ const tabs = document.querySelectorAll(`nav ul li`)
 }) */
 
 const whoIAm = [
-    "a front-end developer",
-    "loved by God",
-    `living on a tight income from relatives`,
-    "a South Sudanese",
-    "an introvert",
-    "a Coder",
-    `interested in web design`,
-    `struggling to make a difference in somebody's life`,
-    `Love CSS`,
-    `a refugee`,
-    `a university student (about)`,
-    "a KUKUtyo",
-    `an indecisive man`,
-    "a Makerere student",
-    `a JavaScript coder`,
-    `a Markdown enthusiast`,
-    "a S6 leaver",
-    "honest",
-    `from Lorijo village in Lire Payam`,
-    `residing in doma village, Palorinya Refugee settlement camp`,
-    `a student from Ebenezer Baptist secondary school Itula`,
-    `a briliant-academically student`,
-    `an electrical engineer (dream)`,
-    `${Math.round(
-        (new Date() - new Date(2002, 6, 30)) / 3600000 / 24 / 366
-    )} years old`
+  "a front-end developer",
+  "loved by God",
+  `living on a tight income from relatives`,
+  "a South Sudanese",
+  "an introvert",
+  "a Coder",
+  `interested in web design`,
+  `struggling to make a difference in somebody's life`,
+  `Love CSS`,
+  `a refugee`,
+  `a university student (about)`,
+  "a KUKUtyo",
+  `an indecisive man`,
+  "a Makerere student",
+  `a JavaScript coder`,
+  `a Markdown enthusiast`,
+  "a S6 leaver",
+  "honest",
+  `from Lorijo village in Lire Payam`,
+  `residing in doma village, Palorinya Refugee settlement camp`,
+  `a student from Ebenezer Baptist secondary school Itula`,
+  `a brilliant-academically student`,
+  `an electrical engineer (dream)`,
+  `${Math.round(
+    (new Date() - new Date(2002, 6, 30)) / 3600000 / 24 / 366
+  )} years old`,
 ];
 const placeHolder = document.querySelector(".whoIAm");
 setInterval(() => {
-    placeHolder.textContent = whoIAm[Math.floor(Math.random() * (whoIAm.length +1))]
+  placeHolder.textContent = whoIAm[Math.floor(Math.random() * whoIAm.length)];
 }, 2523);
 
-const hack = document.getElementById('hack');
+const \u03c0 = 256;
+console.log(π);
+const hack = document.getElementById("hack");
 async function fetchData() {
-    try {
-      const a = await fetch(`https://api.viewbits.com/v1/jester?mode=random`);
-      const b = a.json();
-      console.log(`sucessful`);
-        hack.textContent = b
-    } catch (error) {
-        console.log(`errorous`, error);
-    }
-    return b;
+  try {
+    const a = await fetch(`https://api.viewbits.com/v1/jester?mode=random`);
+    const b = a.json();
+    console.log(`successful`);
+    hack.textContent = b;
+    console.log(b);
+  } catch (error) {
+    console.log(`erroneous`, error);
+    hack.textContent = `Error ###`;
   }
-fetchData()
+  return b;
+}
+fetchData();
